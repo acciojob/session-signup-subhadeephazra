be5df4c25573d331c16839955fa824e03ef70b7a
Subhadeep Hazra <124555668+subhadeephazra@users.noreply.github.com>
2023-09-26 16:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const form = document.getElementById("signup-form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password === confirmPassword) {
+        const userData = {
+            name: name,
+            email: email,
+            password: password
+        };
+
+        // Store user data in session storage
+        sessionStorage.setItem("user", JSON.stringify(userData));
+
+        alert("Sign up successful!");
+        form.reset();
+    } else {
+        alert("Passwords do not match");
+    }
+});
